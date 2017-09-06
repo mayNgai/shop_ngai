8702eae6bc336b4b7b6c4326a19182e918762bd9
may <sasithorn.ne54@sau.ac.th>
2017-09-07 01:33:43 +0700

    update sql

--- shop_ngai/shop_ngai.sql
+++ shop_ngai/shop_ngai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 31, 2017 at 11:54 AM
+-- Generation Time: Sep 06, 2017 at 08:32 PM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.1.4
 
@@ -59,14 +59,14 @@ CREATE TABLE `detail_order` (
   `id` int(11) NOT NULL,
   `o_id` int(11) DEFAULT NULL,
   `do_size` double DEFAULT NULL,
-  `do_amount` bigint(20) DEFAULT NULL
+  `do_stock` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `detail_order`
 --
 
-INSERT INTO `detail_order` (`id`, `o_id`, `do_size`, `do_amount`) VALUES
+INSERT INTO `detail_order` (`id`, `o_id`, `do_size`, `do_stock`) VALUES
 (1, 1, 37, 10),
 (2, 1, 38, 10),
 (3, 1, 39, 10),
@@ -81,7 +81,17 @@ INSERT INTO `detail_order` (`id`, `o_id`, `do_size`, `do_amount`) VALUES
 (12, 3, 38, 10),
 (13, 3, 39, 10),
 (14, 3, 40, 10),
-(15, 3, 41, 10);
+(15, 3, 41, 10),
+(16, 4, 28, 10),
+(17, 4, 29, 10),
+(18, 4, 30, 10),
+(19, 4, 31, 10),
+(20, 4, 32, 10),
+(21, 5, 28, 10),
+(22, 5, 29, 10),
+(23, 5, 30, 10),
+(24, 5, 31, 10),
+(25, 5, 32, 10);
 
 -- --------------------------------------------------------
 
@@ -130,10 +140,11 @@ CREATE TABLE `member` (
 --
 
 INSERT INTO `member` (`m_id`, `m_password`, `m_authentication`, `m_status`, `m_tel`, `m_email`, `m_user_id`, `m_date_create`, `m_first_name`, `m_last_name`) VALUES
-(1, '1234', 'user', 0, '0123456789', 'aa@aa.a', 0, '2017-08-23 08:47:07', 'a', 'aa'),
+(1, '1234', 'user', 0, '0123456789', 'aa@aa.a', 0, '2017-08-23 08:47:07', 'a', 'aaa'),
 (2, '1111', 'user', 0, '0111111110', 'bb@bb.b', 0, '2017-08-28 00:00:00', 'b', 'bbb'),
 (3, '2222', 'user', 0, '0744121453', 'cc@cc.c', 0, '2017-08-28 00:00:00', 'c', 'ccc'),
-(4, '4444', 'user', 0, '0142745230', 'dd@dd.d', 0, '2017-08-28 00:00:00', 'dd', 'dddd');
+(4, '4444', 'user', 0, '0142745230', 'dd@dd.d', 0, '2017-08-28 00:00:00', 'dd', 'dddd'),
+(5, '11111111', 'user', 0, '0999999999', 'qq@qq.q', 0, '2017-09-06 19:54:53', 'qq', 'qqq');
 
 -- --------------------------------------------------------
 
@@ -145,22 +156,21 @@ CREATE TABLE `order` (
   `o_id` int(11) NOT NULL,
   `o_name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
   `o_c_id` int(11) DEFAULT NULL,
-  `o_vendor_id` int(11) DEFAULT NULL,
+  `o_vender_id` int(11) DEFAULT NULL,
   `o_start_date` datetime DEFAULT NULL,
   `o_end_date` datetime DEFAULT NULL,
   `o_price` int(11) DEFAULT NULL,
   `o_discount` int(11) DEFAULT NULL,
   `o_ratting` int(11) DEFAULT NULL,
-  `o_amount` int(11) DEFAULT NULL,
-  `o_type` int(11) DEFAULT NULL,
+  `o_stock` int(11) DEFAULT NULL,
+  `o_category` int(11) DEFAULT NULL,
+  `o_condition` int(10) DEFAULT NULL,
   `o_like` bigint(20) DEFAULT NULL,
-  `o_price_ship` int(11) DEFAULT NULL,
+  `o_wholesale_price` int(11) DEFAULT NULL,
   `o_brand` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
   `o_ship_from` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
   `o_detail` text COLLATE utf8_unicode_ci,
   `o_count_ratting` bigint(20) DEFAULT NULL,
-  `o_lat` float DEFAULT NULL,
-  `o_lan` float DEFAULT NULL,
   `o_pic_name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -168,10 +178,12 @@ CREATE TABLE `order` (
 -- Dumping data for table `order`
 --
 
-INSERT INTO `order` (`o_id`, `o_name`, `o_c_id`, `o_vendor_id`, `o_start_date`, `o_end_date`, `o_price`, `o_discount`, `o_ratting`, `o_amount`, `o_type`, `o_like`, `o_price_ship`, `o_brand`, `o_ship_from`, `o_detail`, `o_count_ratting`, `o_lat`, `o_lan`, `o_pic_name`) VALUES
-(1, 'รองเท้า Convert สีขาว', 1, 1, '2017-08-28 00:00:00', '2017-08-28 00:00:00', 2000, 10, 0, 50, 1, 0, 0, 'Convert', 'bangna', 'size 37-42\r\ncolor ขาว', 0, 13.666, 100.581, 'a_convert_white_01.JPG'),
-(2, 'รองเท้า Convert สีดำ', 1, 1, '2017-08-28 00:00:00', '2017-08-28 00:00:00', 2000, 10, 0, 50, 1, 0, 0, 'Convert', 'bangna', 'size 37-41\r\ncolor ดำ', 0, 13.666, 100.581, 'a_convert_black_01.JPG'),
-(3, 'รองเท้า Convert สีกรม', 1, 1, '2017-08-28 00:00:00', '2017-08-28 00:00:00', 2000, 10, 0, 50, 1, 0, 0, 'Convert', 'bangna', 'size 37-41\r\ncolor กรม', 0, 13.666, 100.581, 'a_convert_navy_01.JPG');
+INSERT INTO `order` (`o_id`, `o_name`, `o_c_id`, `o_vender_id`, `o_start_date`, `o_end_date`, `o_price`, `o_discount`, `o_ratting`, `o_stock`, `o_category`, `o_condition`, `o_like`, `o_wholesale_price`, `o_brand`, `o_ship_from`, `o_detail`, `o_count_ratting`, `o_pic_name`) VALUES
+(1, 'รองเท้า Convert สีขาว', 1, 1, '2017-08-28 00:00:00', '2017-08-28 00:00:00', 2000, 10, 0, 50, 1, 1, 0, 0, 'Convert', 'bangna', 'size 37-42\r\ncolor ขาว', 0, 'a_convert_white_01.JPG'),
+(2, 'รองเท้า Convert สีดำ', 1, 1, '2017-08-28 00:00:00', '2017-08-28 00:00:00', 2000, 10, 0, 50, 1, 1, 0, 0, 'Convert', 'bangna', 'size 37-41\r\ncolor ดำ', 0, 'a_convert_black_01.JPG'),
+(3, 'รองเท้า Convert สีกรม', 1, 1, '2017-08-28 00:00:00', '2017-08-28 00:00:00', 2000, 10, 0, 50, 1, 1, 0, 0, 'Convert', 'bangna', 'size 37-41\r\ncolor กรม', 0, 'a_convert_navy_01.JPG'),
+(4, 'กางเกง Mc สีดำ', 1, 2, '2017-09-04 00:00:00', '2017-09-04 00:00:00', 1200, 10, 0, 50, 1, 1, 0, 0, 'Mc', 'jatujak', 'size 28-32', 0, 'Mc-Jeans-Slim-Fit-black_01.JPG'),
+(5, 'กางเกง Mc สีกรม', 1, 2, '2017-09-04 00:00:00', '2017-09-04 00:00:00', 1200, 10, 0, 50, 1, 1, 0, 0, 'Mc', 'jatujak', 'size 28-32', 0, 'Mc-Jeans-Slim-Fit-navy_01.PNG');
 
 -- --------------------------------------------------------
 
@@ -198,7 +210,11 @@ INSERT INTO `picture` (`id`, `o_id`, `p_name`, `p_detail`) VALUES
 (5, 2, 'a_convert_black_01.JPG', NULL),
 (6, 2, 'a_convert_black_02.JPG', NULL),
 (7, 3, 'a_convert_navy_01.JPG', NULL),
-(8, 3, 'a_convert_navy_02.JPG', NULL);
+(8, 3, 'a_convert_navy_02.JPG', NULL),
+(9, 4, 'Mc-Jeans-Slim-Fit-black_01.JPG', NULL),
+(10, 4, 'Mc-Jeans-Slim-Fit-black_02.JPG', NULL),
+(11, 5, 'Mc-Jeans-Slim-Fit-navy_01.PNG', NULL),
+(12, 5, 'Mc-Jeans-Slim-Fit-navy_02.JPG', NULL);
 
 --
 -- Indexes for dumped tables
@@ -209,12 +225,6 @@ INSERT INTO `picture` (`id`, `o_id`, `p_name`, `p_detail`) VALUES
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`c_id`);
-
---
--- Indexes for table `detail_order`
---
-ALTER TABLE `detail_order`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `discount`
@@ -229,18 +239,6 @@ ALTER TABLE `member`
   ADD PRIMARY KEY (`m_id`);
 
 --
--- Indexes for table `order`
---
-ALTER TABLE `order`
-  ADD PRIMARY KEY (`o_id`);
-
---
--- Indexes for table `picture`
---
-ALTER TABLE `picture`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -250,11 +248,6 @@ ALTER TABLE `picture`
 ALTER TABLE `category`
   MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
--- AUTO_INCREMENT for table `detail_order`
---
-ALTER TABLE `detail_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
---
 -- AUTO_INCREMENT for table `discount`
 --
 ALTER TABLE `discount`
@@ -263,17 +256,7 @@ ALTER TABLE `discount`
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `m_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `order`
---
-ALTER TABLE `order`
-  MODIFY `o_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `picture`
---
-ALTER TABLE `picture`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;COMMIT;
+  MODIFY `m_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
